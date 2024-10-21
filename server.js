4cfa5f041ef1d1521cb702e7f1bea8aef6d04a98
Ksenia Sitnikova <xenyasitnikova88@gmail.com>
2024-10-21 12:40:16 +0300

    feat: add get and post users data

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ export const requestHandler = (request, response) => {
         return false;
     }
     const match = pathname.match(/^\/api\/users\/(\d+)$/)
-    response.setHeader('Content-Type', 'application/json')
+
     if (match) {
         const id = match[1];
         const user = findUser(id)
@@ -24,26 +24,46 @@ export const requestHandler = (request, response) => {
                 response.writeHead(200, {'Content-Type': 'application/json'})
                 response.end(JSON.stringify(user))
             } else {
-                response.writeHead(404, {'Content-Type': 'text/html'})
-                response.end(`<h1>User doesn't exist</h1>`)
+                response.writeHead(404, {'Content-Type': 'text/plain'})
+                response.end('User doesn\'t exist')
             }
             break;
             case 'POST':
-                response.writeHead(201, {'Content-Type': 'application/json'})
-                response.end(JSON.stringify(user))
+                let body = ''
+                request.on('data', chunk => {
+                    body += chunk.toString()
+                })
+                request.on('end', () => {
+                    try {
+                        const data = JSON.parse(body)
+                        users.push(data)
+                        response.writeHead(201, {'Content-Type': 'application/json'})
+                        response.end(JSON.stringify(users))
+                    } catch (error) {
+                        response.writeHead(400, {'Content-Type': 'text/plain'})
+                        response.end('invalid JSON')
+                    }
+                })
             break;
             case 'PUT':
-            response.writeHead(200, {'Content-Type': 'application/json'})
-            response.end(JSON.stringify(user))
+                response.writeHead(200, {'Content-Type': 'application/json'})
+                response.end(JSON.stringify(user))
             break;
             case 'DELETE':
-            response.writeHead(200, {'Content-Type': 'application/json'})
-            response.end(JSON.stringify(user))
+                response.writeHead(200, {'Content-Type': 'application/json'})
+                response.end(JSON.stringify(user))
+            break;
+            default:
+                response.writeHead(405, { 'Content-Type': 'text/plain' });
+                response.end('Method not allowed');
             break;
         }
         
     } else if (request.url === "/api/users") {
-        response.writeHead(200)
+        response.writeHead(200, {'Content-Type': 'application/json'})
         response.end(JSON.stringify(users))
-    }    
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.end('page does not exist');
+    }   
 }
